15e8dc92a08e1dda7e2fe09b0f23583bf55a3e45
momokaiwase <momoka.iwase03@gmail.com>
2025-04-15 16:53:14 -0400

    Initial Commit

---
+++ memora/memoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoraApp.swift
+//  memora
+//
+//  Created by Momoka Iwase on 2025/04/15.
+//
+
+import SwiftUI
+
+@main
+struct memoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
